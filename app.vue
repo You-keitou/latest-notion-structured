b47a0d7d8f99b5e60f9e64e4188b54c5cf449b37
jinyang <youkeitou327@gmail.com>
2024-11-03 19:27:43 +0900

    feat: google auth loginができるように

--- app.vue
+++ app.vue
@@ -1,6 +1,43 @@
+<script>
+definePageMeta({
+  colorMode: "dark",
+});
+
+const isOffline = ref(false);
+
+onMounted(() => {
+  window.addEventListener("offline", () => {
+    isOffline.value = true;
+  });
+  window.addEventListener("online", () => {
+    isOffline.value = false;
+  });
+});
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLayout>
+      <NuxtLoadingIndicator></NuxtLoadingIndicator>
+      <NuxtPage />
+      <UNotifications />
+
+      <select
+        v-model="$colorMode.preference"
+        class="border-l border-t fixed right-0 bottom-0 z-50 px-2 py-1 rounded-tl-lg border-gray-300 dark:border-stone-700"
+      >
+        <option value="system">System</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+      </select>
+    </NuxtLayout>
   </div>
 </template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600&display=swap");
+
+* {
+  font-family: "Plus Jakarta Sans", sans-serif;
+}
+</style>
---
+++ pages/sign-up.vue
@@ -0,0 +1,163 @@
+<script setup lang="ts">
+import { BaseError, useErrorHandler } from "~/composables/use-error-handler";
+import {
+  type SchemaSignUpValidation,
+  signUpValidation,
+} from "~/utils/formValidation";
+import type { FormSubmitEvent } from "#ui/types";
+
+definePageMeta({
+  middleware: "guest",
+});
+
+useSeoMeta({
+  title: "Sign Up - Notion Structured",
+});
+
+const { auth } = useSupabaseClient();
+const { errorHandler } = useErrorHandler();
+
+const form = reactive({
+  email: undefined,
+  password: undefined,
+});
+
+const isLoading = ref(false);
+
+/**
+ * Sign up with credential.
+ *
+ * @param {FormSubmitEvent<SchemaSignUpValidation>} event - The form submit event.
+ * @return {Promise<void>} A promise that resolves when the sign-up process is completed.
+ */
+const signUpWithCredential = async (
+  event: FormSubmitEvent<SchemaSignUpValidation>
+) => {
+  try {
+    isLoading.value = true;
+
+    const signUp = await auth.signUp({
+      email: form.email ?? "",
+      password: form.password ?? "",
+    });
+
+    if (signUp.error) {
+      throw new BaseError(signUp.error.status, signUp.error.message);
+    }
+
+    navigateTo("/dashboard");
+    isLoading.value = false;
+  } catch (error) {
+    isLoading.value = false;
+    errorHandler(error as BaseError);
+  }
+};
+
+/**
+ * Sign up with the specified provider.
+ *
+ * @param {string} provider - The provider to sign up with (either 'GITHUB' or 'GOOGLE').
+ * @return {Promise<void>} A promise that resolves when the sign-up process is complete.
+ */
+const signUpWithProvider = async (provider: "GITHUB" | "GOOGLE") => {
+  try {
+    isLoading.value = true;
+
+    let signUp = null;
+
+    if (provider === "GITHUB") {
+      signUp = await auth.signInWithOAuth({
+        provider: "github",
+        options: {
+          redirectTo: `${window.location.origin}/confirm`,
+        },
+      });
+    }
+
+    if (provider === "GOOGLE") {
+      signUp = await auth.signInWithOAuth({
+        provider: "google",
+        options: {
+          redirectTo: `${window.location.origin}/confirm`,
+        },
+      });
+    }
+
+    if (signUp?.error) {
+      isLoading.value = false;
+      throw new BaseError(signUp.error.status, signUp.error.message);
+    }
+
+    navigateTo("/");
+    isLoading.value = false;
+  } catch (error) {
+    isLoading.value = false;
+    errorHandler(error as BaseError);
+  }
+};
+</script>
+
+<template>
+  <div>
+    <div class="container mx-auto">
+      <div class="flex h-[100vh] w-full justify-center items-center">
+        <div class="w-10/12 md:w-8/12 lg:w-4/12 xl:w-3/12">
+          <UCard>
+            <UForm
+              :schema="signUpValidation"
+              :state="form"
+              class="space-y-2"
+              @submit="signUpWithCredential"
+            >
+              <div class="space-y-4">
+                <div class="flex justify-center">
+                  <img src="/icon.svg" class="h-[80px]" />
+                </div>
+
+                <p class="text-lg font-bold text-center">
+                  Register an account by entering your email and password.
+                </p>
+
+                <UFormGroup label="Email" name="email">
+                  <UInput v-model="form.email" />
+                </UFormGroup>
+
+                <UFormGroup label="Password" name="password">
+                  <UInput v-model="form.password" type="password" />
+                </UFormGroup>
+
+                <UButton
+                  :loading="isLoading"
+                  :disabled="isLoading"
+                  type="submit"
+                  label="Register"
+                  color="gray"
+                  block
+                />
+              </div>
+              <UDivider label="or" color="gray" orientation="vertical" />
+
+              <div class="space-y-4 flex flex-col justify-center">
+                <UButton
+                  @click="signUpWithProvider('GOOGLE')"
+                  :disabled="isLoading"
+                  color="black"
+                  label="Continue with Google"
+                  icon="i-logos-google-icon"
+                  block
+                />
+              </div>
+            </UForm>
+          </UCard>
+
+          <div class="flex items-center gap-2 justify-center mt-3">
+            <p class="opacity-50">Already have an account?</p>
+            <NuxtLink to="/sign-in" class="underline">Sign in</NuxtLink>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
