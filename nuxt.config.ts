5ff50aad48bc988017024c7c8e895a7c47342064
jinyang <youkeitou327@gmail.com>
2024-12-27 17:46:59 +0900

    feat: dayjs graphragのmoduleを導入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: [
-    '@nuxtjs/supabase',
-    '@nuxt/icon',
-    '@nuxt/ui',
-    '@nuxthub/core'
-  ]
-})
+    "@nuxtjs/supabase",
+    "@nuxt/icon",
+    "@nuxt/ui",
+    "@nuxthub/core",
+    "dayjs-nuxt",
+    "@nuxtjs/apollo",
+  ],
+  colorMode: {
+    preference: "system",
+    fallback: "light",
+  },
+  apollo: {
+    clients: {
+      default: {
+        httpEndpoint: `${process.env.SUPABASE_URL}/graphql/v1`,
+      },
+    },
+  },
+});
